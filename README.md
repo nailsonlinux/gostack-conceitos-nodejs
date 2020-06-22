c595c8a3f97bcbbd68c40b735bd078b0f4f22973
Nailson Landim <nailsonlinux@gmail.com>
2020-06-22 12:34:59 -0300

    Job done, Passing all tests

---
+++ README.md
@@ -0,0 +1,7 @@
+# RocketSeat GoStack - NodeJS Concepts
+
+## What is this
+
+This is a simple NodeJS Express Application, that registers Repositories, used technologies and how many likes did it got.
+
+As is a dead simple app, it does not persist on a database. It keeps on memory and once the server is reloaded data is lost.
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,82 @@ app.use(cors());
 
 const repositories = [];
 
+function validateRepositoryId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Invalid Repository ID" });
+  }
+
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  // List all projects-repos
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  // Save a new project-repo - Receives on request: title, url, techs
+  const { title, url, techs } = request.body;
+  let likes = 0;
+  const repository = { id: uuid(), title, url, techs, likes };
+  repositories.push(repository);
+  return response.status(200).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateRepositoryId, (request, response) => {
+  // Update a project-repo
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository Not Found" });
+  }
+
+  likes = repositories[repositoryIndex].likes;
+  const repository = { id, title, url, techs, likes };
+  repositories[repositoryIndex] = repository;
+  return response.status(200).json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateRepositoryId, (request, response) => {
+  // Destroy a project-repo
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository Not Found" });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+app.post(
+  "/repositories/:id/like",
+  validateRepositoryId,
+  (request, response) => {
+    // Crate a Like on a repo
+    const { id } = request.params;
+    const repositoryIndex = repositories.findIndex(
+      (repository) => repository.id === id
+    );
+
+    if (repositoryIndex < 0) {
+      return response.status(400).json({ error: "Repository Not Found" });
+    }
+
+    repositories[repositoryIndex].likes += 1;
+
+    return response.status(200).json(repositories[repositoryIndex]);
+  }
+);
 
 module.exports = app;
